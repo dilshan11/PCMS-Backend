0586e9c0257c455a205577762dc452234bfbfc85
Sachithra Dilshan <itismesachithra@gmail.com>
2019-12-27 22:49:35 +0530

    ipa is changed

--- models/ipa.js
+++ models/ipa.js
@@ -5,7 +5,9 @@ const Ipa=mongoose.model('ipa',new mongoose.Schema({
     project:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Project'
-    }
+    },
+    subtopic:Array,
+    maintopic:Array
 })
 );
 module.exports.Ipa=Ipa;
--- router/ipa.js
+++ router/ipa.js
@@ -3,13 +3,26 @@ const {Ipa}=require('../models/ipa');
 const router=express.Router();
 
 router.post('/save',async(req,res)=>{
-    let ipa=new Ipa({
-        basecontent:req.body.base,
-        project:req.body.project
-    });
+   
     
-    let result=await Ipa.deleteOne({project:req.body.project});
-     result=await ipa.save();
+    let onedoc=await Ipa.find({project:req.body.project});
+    if(onedoc.length==0){
+         let ipa=new Ipa({
+        basecontent:req.body.base,
+        project:req.body.project,
+        subtopic:req.body.subtopic,
+        maintopic:req.body.maintopic
+    });
+      let result= await ipa.save();
+      res.send(result[0]);
+      return
+    }
+    
+    onedoc=onedoc[0];
+    onedoc.basecontent=req.body.base;
+    onedoc.subtopic=req.body.subtopic;
+    onedoc.maintopic=req.body.maintopic;
+    let result=await onedoc.save();
     res.send(result);
 });
 
